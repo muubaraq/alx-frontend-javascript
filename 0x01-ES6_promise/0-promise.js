d650c1cfa5c5265e69e3ca0876ee1e7f5706988b
mubaraq <paynedaze@gmail.com>
2022-12-20 18:54:21 +0100

    0-promise modified

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,8 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Make a request to the API
-      const response = requestAPI();
-  
-      // If the request was successful, resolve the Promise with the response
-      if (response.status === 200) {
-        resolve(response);
-      } else {
-        // If the request was not successful, reject the Promise with an error message
-        reject(new Error('Failed to get response from API'));
-      }
-    });
+export default function getResponseFromAPI() {
+    const myPromise = new Promise(
+      (resolve) => {
+        resolve();
+      },
+    );
+    return myPromise;
   }
-  
