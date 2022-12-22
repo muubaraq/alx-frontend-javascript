f07deccb132ea7a32809222f15a0ff7a9bc8e826
mubaraq <paynedaze@gmail.com>
2022-12-22 13:27:07 +0100

    modified

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,28 +1,26 @@
-class Car {
+export default class Car {
     constructor(brand, motor, color) {
+      // Create objs
       this._brand = brand;
       this._motor = motor;
       this._color = color;
     }
   
-    get brand() {
-      return this._brand;
-    }
-  
-    get motor() {
-      return this._motor;
-    }
-  
-    get color() {
-      return this._color;
-    }
+    // Methods
   
     cloneCar() {
-      const clone = new Car();
-      clone._brand = this._brand;
-      clone._motor = this._motor;
-      clone._color = this._color;
+      const NewObj = this.constructor[Symbol.species] || this.constructor;
+      const clone = new NewObj();
       return clone;
     }
-  }
   
+    // Setters
+  
+    // Getters
+  }
+  /* class TestCar extends Car {};
+  const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+  const tc2 = tc1.cloneCar();
+  console.log(tc1 instanceof TestCar);
+  console.log(tc2 instanceof TestCar);
+  console.log(tc1 == tc2); */
